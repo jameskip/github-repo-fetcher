f8a885cf1287d638adef686ed3e6fec825c179bc
James Kip <jameskip@me.com>
2017-03-07 22:13:33 -0600

    Implementing - auto refresh of table

--- server/index.js
+++ server/index.js
@@ -27,7 +27,6 @@ app.post('/repos/import', function (req, res, next) {
     knex('repos').insert(insert)
     .then(function (req, res) {
       res.send(200);
-
     })
     .catch(function(err) {
       console.error(JSON.stringify(err));
